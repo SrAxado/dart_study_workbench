953ff829e4f51ac54921f6e4b86cb961bc40bc7d
Axado <sr.axado@gmail.com>
2024-03-15 01:28:09 +0000

    Patterns_full

--- bin/dart_study_workbench.dart
+++ bin/dart_study_workbench.dart
@@ -190,6 +190,129 @@ void main(List<String> arguments) {
     case var s?:
       // 's' has type non-nullable String here.
   }
+
+  /// Null-assert
+  /// Null-assert patterns match first if the object is not null, then on the value. They permit non-null values
+  /// to flow through, but throw if the matched value is null.
+  /// To ensure null values are not silently treated as match failures, use a null-assert pattern while matching
+  List<String?> row = ['user', null];
+  switch (row) {
+    case ['user', var name]:
+      // 'name' is a non-nullable string here.
+  }
+  // To eliminate null values from variable declaration patterns, use the null-assert pattern.
+  (int?, int?) position = (2, 3);
+  var (x!, y!) = position; // post operator ! - non-null assertion operator
+
+
+  /// To match when the value is null, use the constant pattern null.
+  /// 
+  /// Contant
+  /// 123, null, 'string, math.pi, SomeClass.constant, const Thing(1, 2), const (1+2)
+  switch (number) {
+    // Matches if 1 == number
+    case 1: // ...
+    case null: /// ...
+  }
+
+  /// Variable
+  /// Variable patterns bind new variables to values that have been matched or destructured.
+  switch ((1, 2)) {
+    case (var a, var b):
+      // 'a' and 'b' are in scope in the case body.
+      print('($a, $b)');
+    case (int a, String b):
+      // Does not match -- will never match
+      print('($a, $b)');
+
+    /// identifier
+    case (_, var b):
+      /// wildcard identifier in any context: matches any value and discards it
+      /// case [_, var y, _]: print('The middle element is $y');
+      print('The second pair-element is $b');
+  }
+
+  /// Parenthesized (subpattern)
+  /// Parentheses in a pattern let you control pattern precende and insert a lower-precedence pattern where a higher precedence one is expected.
+  var (xP, yP, zP) = (true, true, false);
+  print(xP || yP && zP); // prints true --> xP OR (yP && zP)
+  print((xP || yP) && zP); // prints false --> (xP || yP) AND zP
+
+  /// List [subpattern1, subpattern2]
+  /// A list pattern matches values that implement List, and then recursively matches its subpatterns against the list's elements to destructure them by position
+  switch (obj) {
+    case (3, 7):
+      print(obj);
+    case [a, b]:
+      print('$a, $b');
+  }
+  /// List patterns require that the number of elements in the pattern match the entire list.
+  
+  /// Rest element
+  /// The use of a rest element as a place holder accounts for any number of elements in a list.
+  /// List patterns can contain one rest element (...) which allows matching list of arbitratry lengths.
+  var [aRe, bRe, ..., cRe, dRe] = [1, 2, 3, 4, 5, 6, 7];
+  print('$aRe $bRe $cRe $dRe');
+
+  /// A rest element can also have a supattern that collects elements that don't match the other supatterns in the list into a new list
+  var [aR, bR, ...rest, cR, dR] = [1, 2, 3, 4, 5, 6, 7];
+  print('$aR $bR $cR $dR -- $rest'); // 1 2 6 7 -- [3, 4, 5]
+
+  /// Map {'key': subpattern1, someConst: supattern2}
+  /// Map patterns match values that implement Map 
+  /// and then recursively match its subpattterns agains the map's keys to destructure them.
+  /// Map patterns don't require the pattern to match the entire map; a map ignores any keys that the map contains that aren't matched by the pattern.
+  print({'key': 13});
+  switch ({'key': 13}) {
+    case {'chave': 13}:
+      print('Match!!');
+    case {'key': 13}:
+      print('Middle case match!');
+    case {null: _}:
+      print('General Match!!');
+  }
+  
+  /// Record (subpattern1, subpattern2) --- (x: subpattern1, y: subpattern2)
+  /// Record patterns match a record and destructure its fields.
+  /// Record patterns require that the pattern match the entire record!
+  /// To destructure a record with named fields using a pattern, include the field names in the pattern
+  var (myString: foo, myNumber: bar) = (myString: 'string', myNumber: 3);
+  print('$foo -- $bar');
+
+  /// The getter name can be ommited and inferred from the varable pattern or identifier pattern in the field subpattern.
+  /// The following pairs are each equivalent
+  /// 
+  // Record pattern with variable subpatterns
+  // var (untyped: untype, typed: int typed) = record;
+  // var (:untyped, :int typed) = record;
+  // 
+  // switch (record) {
+  //   case(untyped: var untyped, typed: int typed): // ...
+  //   case (:var untyped, :int typed): // ...
+  // }
+  // 
+  // Record with null-check and null-assert subpatterns
+  // switch (record) {
+      // case (checked: var checked?, asserted: var asserted!): // ...
+      // case (:var checked?, :var asserted!): // ...
+  // }
+  // 
+  // Record pattern with cast subpattern
+  // var (untyped: untyped as int, typed: typed as String) = record;
+  // var (:untyped as int, :typed as String) = record;
+
+  /// Object
+  /// Object patterns check the matched value agains a given named type to destructure data using getters on the object's properties.
+  // switch (shape) {
+  //   // Matches if shape is of type Rect and then against the properties of Rect.
+  //   case Rect(width: var w, height: var h): // ...
+  // }
+  // 
+  // The getter name can be omitted and inferred from the variable pattern and identifier pattern in the field subpattern
+  // Bind new variables x an y to the values of Point's x an y properties
+  // var Point(:x, :y) = Point(1, 2);
+  /// Object patterns don't require the parttern to match the entire object.
+  
 }
 
 class Foo {
